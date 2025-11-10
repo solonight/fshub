0424ba94015feacac4531a0389bf69708ffbf6a9
solonight <thenewpage500@gmail.com>
2025-11-10 19:09:13 +0100

    nav backgoud color update

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -16,7 +16,7 @@ export default function Authenticated({
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
+            <nav className="border-b border-gray-100 bg-[#1D1B1B]">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -6,14 +6,20 @@ export default function Dashboard({ auth }: PageProps) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-[#2596be] leading-tight">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
+            <div className="py-12 bg-[#1D1B1B] min-h-screen">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    <div className="bg-[#D9D9D9] overflow-hidden shadow-sm sm:rounded-lg border border-[#2596be]">
+                        <div className="p-6 text-[#1D1B1B] font-semibold">
+                            You're logged in!
+                        </div>
                     </div>
                 </div>
             </div>
