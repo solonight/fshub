241b339d875c6ef30a2298583bca8827c0a52172
solonight <thenewpage500@gmail.com>
2025-12-06 20:29:39 +0100

    manage your stocks medium size section fixed

--- resources/js/Pages/StockDashboard.tsx
+++ resources/js/Pages/StockDashboard.tsx
@@ -481,7 +481,7 @@ export default function StockDashboard({
                                 No stocks found.
                             </div>
                         ) : (
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
                                 {fabricStocks &&
                                     fabricStocks.data &&
                                     fabricStocks.data.map((stock: any) => (
